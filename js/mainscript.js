ffa60d92a0113653fd93a2d6a9aef8fb0e79e7b4
Viktor Kirilenko <vkirilenko@bellintegrator.ru>
2017-07-03 15:22:07 +0300

    c2

--- js/mainscript.js
+++ js/mainscript.js
@@ -2,11 +2,31 @@
  * Created by VKirilenko on 30.06.2017.
  */
 
+//элементы страницы
+var btAdd = document.vehicleForm.vehicleAdd;
+var radio = document.vehicleForm.vehicleType;
+var vehicleName = document.vehicleForm.vehicleName;
+var vehicleWeight = document.vehicleForm.vehicleWeight;
+var vehicleDescription = document.vehicleForm.vehicleDescription;
+var vehicleListOptionTemplate = _.template(document.getElementById('vehicleListOption').innerHTML);
+var vehicleList = document.getElementById('vehicleList');
+var vehicleDetails = document.getElementById('vehicleDetails');
+var vehicleDetailsTemplate = _.template(document.getElementById('vehicleDetailsTemplate').innerHTML);
+
+
 //Базовый класс
 function Vehicle(name, description) {
     // сохранение параметров name, description
-    Vehicle.prototype.name = name;
-    Vehicle.prototype.description = description;
+    var _name = name;
+    var _description = description;
+
+    this.getName = function() {
+        return _name;
+    }
+
+    this.getDescription = function() {
+        return _description;
+    }
 }
 
 
@@ -14,39 +34,107 @@ function Vehicle(name, description) {
 function Truck(name, description, carrying) {
     Vehicle.call(this, name, description);
 
+    Truck.prototype.vehicleType = "легковая";
+    Truck.prototype.vehicleSpecificName = "Масса";
+    Truck.prototype.vehicleSpecificUnit = "т";
+
+    this.getSpecificValue = function(){
+        return _carrying;
+    }
+
+    var _carrying = carrying;
+
     // информация для отображения в списке машин
     this.showLabel = function () {
-        return "";
+        return vehicleListOptionTemplate(this);
     }
 
     // информация для отображения под списком машин,
     // при выборе строки в списке
-//     this.showInfo
-// :
-//     function () {
-//         return “Тип: ”
-//         +this.name + “<
-//         br >” +
-//               “...”;
-//     }
+    Truck.prototype.showInfo = function () {
+        return vehicleDetailsTemplate(this);
+    }
 }
+Truck.prototype = Object.create(Vehicle.prototype);
 
 //Для легковых:
-function car(name, description, velocity) {
-    //
+function Car(name, description, velocity) {
+    Vehicle.call(this, name, description);
 
-//     this.showLabel =
-// ...
-//
-//     this.showInfo
-// :
-//     function () {
-//         return “Тип: ”
-//         +this.name + “<
-//         br >” +
-//               “...”;
-//     }
+    Car.prototype.vehicleType = "грузовая";
+    Car.prototype.vehicleSpecificName = "Скорость";
+    Car.prototype.vehicleSpecificUnit = "км/ч";
+
+    this.getSpecificValue = function () {
+        return _velocity;
+    }
+
+    var _velocity = velocity;
+
+    // информация для отображения в списке машин
+    this.showLabel = function () {
+        return vehicleListOptionTemplate(this);
+    }
+
+    Car.prototype.showInfo = function () {
+        return vehicleDetailsTemplate(this);
+    }
 }
+Car.prototype = Object.create(Vehicle.prototype);
 
 //Для хранения списка машин использовать один объет – массив.
 var vehicles = [];
+
+btAdd.onclick = function btAdd(){
+    var type = radio.value;
+    if (type == ""){
+        alert("Выберите тип машины");
+        return;
+    }
+
+    var vehicleNameVal = vehicleName.value, vehicleDescriptionVal = vehicleDescription.value, vehicleWeightVal = vehicleWeight.value;
+
+    var vehicle = (type == "Truck") ?
+        new Truck(vehicleNameVal, vehicleDescriptionVal, vehicleWeightVal) :
+        new Car(vehicleNameVal, vehicleDescriptionVal, vehicleWeightVal);
+
+    vehicles.push({
+        vehicle: vehicle,
+        onClick: vehicle.showInfo.bind(vehicle)
+    });
+
+    var template = document.createRange().createContextualFragment(vehicle.showLabel.call(vehicle));
+    vehicleList.appendChild(template);
+
+    // обнудяем поля
+    vehicleName.value = vehicleDescription.value = vehicleWeight.value = "";
+
+}
+
+vehicleList.onchange = function(){
+    vehicleDetails.innerHTML = vehicles[vehicleList.selectedIndex].onClick();
+}
+
+
+
+
+
+//for test****************
+function InitData() {
+    var v = new Truck("Mersedes", "description1 ", "3");
+    vehicles.push({
+        vehicle: v,
+        onClick: v.showInfo.bind(v)
+    });
+    var template = document.createRange().createContextualFragment(v.showLabel());
+    vehicleList.appendChild(template);
+    var v = new Car("Honda", "description2 ", "250");
+    vehicles.push({
+        vehicle: v,
+        onClick: v.showInfo.bind(v)
+    });
+    var template = document.createRange().createContextualFragment(v.showLabel());
+    vehicleList.appendChild(template);
+};
+InitData();
+//end test
